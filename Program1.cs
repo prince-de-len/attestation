de9e84fd3ee00f704780d2ab271ee97e4a23b7a3
prince-de-len <144515902+prince-de-len@users.noreply.github.com>
2024-02-07 13:52:03 +0700

    Try2

---
+++ Program1.cs
@@ -0,0 +1,33 @@
+﻿/******************************************************************************
+https://onlinegdb.com/mtK973kJaF
+Codestyle: https://learn.microsoft.com/ru-ru/dotnet/csharp/fundamentals/coding-style/coding-conventions
+Название: Базовые понятия языка С#
+Автор: Тимофеев Гордей Евгеньевич
+Версия: 1
+*******************************************************************************/
+
+using System;
+
+class Program
+{
+    static int Multiply(int A, int N)
+    {
+        int Result = 1;
+        for (int Index = 0; Index < N; Index++)
+        {
+            Result *= A;
+        }
+        return Result;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Введите число а:");
+        int A = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите число n:");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int Result = Multiply(A, N);
+        Console.WriteLine($"Результат: {Result}");
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,31 @@
+﻿/******************************************************************************
+https://onlinegdb.com/XcMwzStvL
+Codestyle: https://learn.microsoft.com/ru-ru/dotnet/csharp/fundamentals/coding-style/coding-conventions
+Название: Базовые понятия языка С#
+Автор: Тимофеев Гордей Евгеньевич
+Версия: 1
+*******************************************************************************/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число, содержащее более двух цифр");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        if (x < 100)
+        {
+            Console.WriteLine("Число должно быть больше или равно 100");
+            return;
+        }
+
+        string stringX = x.ToString();
+        char removedDigit = stringX[1];
+        string stringXWithoutRemovedDigit = stringX.Substring(0, 1) + stringX.Substring(2);
+        string n = stringXWithoutRemovedDigit + removedDigit;
+
+        Console.WriteLine($"Для введенного x = {x}, n = {n}");
+    }
+}
